3856aab31f5a7e7a1b5afd11d87ee19f8f34384b
Alexis Mathon <Mathonal@gmail.com>
2021-03-27 22:42:25 +0100

    Create jenkinsjob_shellcommand.sh

---
+++ jenkinsjob_shellcommand.sh
@@ -0,0 +1,12 @@
+# to copy paste in jenkins job command
+# 1- clone
+git clone https://github.com/Mathonal/basic-jenkins-testjob
+
+# 2- getting in project repo
+cd basic-jenkins-testjob
+
+# 3- compiling project
+javac Main.java
+
+# 4- run
+java Main
